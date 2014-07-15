50171436b20ca8caa3a47a10a52f0905d0015b9a
Mike Wyatt <wyatt.mike@gmail.com>
2014-07-14 23:11:46 -0300

    get it together jasmine boot.js

--- test/assets/test/boot.js
+++ test/assets/test/boot.js
@@ -1,7 +1,15 @@
 /**
- Starting with version 2.0, this file "boots" Jasmine, performing all of the necessary initialization before executing the loaded environment and all of a project's specs. This file should be loaded after `jasmine.js`, but before any project source files or spec files are loaded. Thus this file can also be used to customize Jasmine for a project.
+ Starting with version 2.0, this file "boots" Jasmine, performing all of the
+ necessary initialization before executing the loaded environment and all of a
+ project's specs. This file should be loaded after `jasmine.js`, but before any
+ project source files or spec files are loaded. Thus this file can also be used
+ to customize Jasmine for a project.
 
- If a project is using Jasmine via the standalone distribution, this file can be customized directly. If a project is using Jasmine via the [Ruby gem][jasmine-gem], this file can be copied into the support directory via `jasmine copy_boot_js`. Other environments (e.g., Python) will have different mechanisms.
+ If a project is using Jasmine via the standalone distribution, this file can
+ be customized directly. If a project is using Jasmine via the [Ruby
+ gem][jasmine-gem], this file can be copied into the support directory via
+ `jasmine copy_boot_js`. Other environments (e.g., Python) will have different
+ mechanisms.
 
  The location of `boot.js` can be specified and/or overridden in `jasmine.yml`.
 
@@ -13,24 +21,30 @@
   /**
    * ## Require &amp; Instantiate
    *
-   * Require Jasmine's core files. Specifically, this requires and attaches all of Jasmine's code to the `jasmine` reference.
+   * Require Jasmine's core files. Specifically, this requires and attaches all
+   * of Jasmine's code to the `jasmine` reference.
    */
   window.jasmine = jasmineRequire.core(jasmineRequire);
 
   /**
-   * Since this is being run in a browser and the results should populate to an HTML page, require the HTML-specific Jasmine code, injecting the same reference.
+   * Since this is being run in a browser and the results should populate to an
+   * HTML page, require the HTML-specific Jasmine code, injecting the same
+   * reference.
    */
   jasmineRequire.html(jasmine);
 
   /**
-   * Create the Jasmine environment. This is used to run all specs in a project.
+   * Create the Jasmine environment. This is used to run all specs in a
+   * project.
    */
   var env = jasmine.getEnv();
 
   /**
    * ## The Global Interface
    *
-   * Build up the functions that will be exposed as the Jasmine public interface. A project can customize, rename or alias any of these functions as desired, provided the implementation remains unchanged.
+   * Build up the functions that will be exposed as the Jasmine public
+   * interface. A project can customize, rename or alias any of these functions
+   * as desired, provided the implementation remains unchanged.
    */
   var jasmineInterface = {
     describe: function(description, specDefinitions) {
@@ -75,7 +89,9 @@
   };
 
   /**
-   * Add all of the Jasmine global/public interface to the proper global, so a project can use the public interface directly. For example, calling `describe` in specs instead of `jasmine.getEnv().describe`.
+   * Add all of the Jasmine global/public interface to the proper global, so a
+   * project can use the public interface directly. For example, calling
+   * `describe` in specs instead of `jasmine.getEnv().describe`.
    */
   if (typeof window == "undefined" && typeof exports == "object") {
     extend(exports, jasmineInterface);
@@ -107,7 +123,8 @@
   /**
    * ## Runner Parameters
    *
-   * More browser specific code - wrap the query string in an object and to allow for getting/setting parameters from the runner user interface.
+   * More browser specific code - wrap the query string in an object and to
+   * allow for getting/setting parameters from the runner user interface.
    */
 
   var queryString = new jasmine.QueryString({
@@ -119,7 +136,9 @@
 
   /**
    * ## Reporters
-   * The `HtmlReporter` builds all of the HTML UI for the runner page. This reporter paints the dots, stars, and x's for specs, as well as all spec names and all failures (if any).
+   * The `HtmlReporter` builds all of the HTML UI for the runner page. This
+   * reporter paints the dots, stars, and x's for specs, as well as all spec
+   * names and all failures (if any).
    */
   var htmlReporter = new jasmine.HtmlReporter({
     env: env,
@@ -131,13 +150,15 @@
   });
 
   /**
-   * The `jsApiReporter` also receives spec results, and is used by any environment that needs to extract the results  from JavaScript.
+   * The `jsApiReporter` also receives spec results, and is used by any
+   * environment that needs to extract the results  from JavaScript.
    */
   env.addReporter(jasmineInterface.jsApiReporter);
   env.addReporter(htmlReporter);
 
   /**
-   * Filter which specs will be run by matching the start of the full name against the `spec` query param.
+   * Filter which specs will be run by matching the start of the full name
+   * against the `spec` query param.
    */
   var specFilter = new jasmine.HtmlSpecFilter({
     filterString: function() { return queryString.getParam("spec"); }
@@ -148,7 +169,8 @@
   };
 
   /**
-   * Setting up timing functions to be able to be overridden. Certain browsers (Safari, IE 8, phantomjs) require this hack.
+   * Setting up timing functions to be able to be overridden. Certain browsers
+   * (Safari, IE 8, phantomjs) require this hack.
    */
   window.setTimeout = window.setTimeout;
   window.setInterval = window.setInterval;
@@ -158,7 +180,10 @@
   /**
    * ## Execution
    *
-   * Replace the browser window's `onload`, ensure it's called, and then run all of the loaded specs. This includes initializing the `HtmlReporter` instance and then executing the loaded Jasmine environment. All of this will happen after all of the specs are loaded.
+   * Replace the browser window's `onload`, ensure it's called, and then run
+   * all of the loaded specs. This includes initializing the `HtmlReporter`
+   * instance and then executing the loaded Jasmine environment. All of this
+   * will happen after all of the specs are loaded.
    */
   var currentWindowOnload = window.onload;
 
